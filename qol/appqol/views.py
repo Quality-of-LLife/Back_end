2646f892ade8b3ac7b4988e6b5597aa2bcc64d53
yyeonzu <tooma22@naver.com>
2022-07-30 03:55:14 +0900

    [JYJ] fix : appqol merge

--- qol/appqol/views.py
+++ qol/appqol/views.py
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
+=======
+from django.shortcuts import render, redirect
+# import requests
+import json
+>>>>>>> Stashed changes
 
 def home(request):
     return render(request, 'index.html')
